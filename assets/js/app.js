0642301021a67a79675d684e8140c418e4b90fb3
Harry Cresswell <studio@harrycresswell.com>
2020-03-21 19:55:24 +0000

    update: change url to production

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,13 @@
 // Yank theme color from localStorage and use it.
 document.documentElement.style.setProperty("--color-brand", localStorage.getItem("userThemeColor"));
+document.documentElement.style.setProperty("--color-brand-opacity", localStorage.getItem("userThemeColor"));
 
 const colorInput = document.querySelector("#choose-theme-color");
 
 colorInput.addEventListener("change", function() {
   // Theme the site!
   document.documentElement.style.setProperty("--color-brand", this.value);
+  document.documentElement.style.setProperty("--color-brand-opacity", this.value);
   // Save the value for next time page is visited.
   localStorage.setItem("userThemeColor", this.value);
 });
